a7a66f06670fd9fc34809de349fcd419dac780a9
Chris Carr <chris.a.carr@gmail.com>
2015-09-12 21:47:32 -0500

    update the urls

--- violatationsdata.sql
+++ violatationsdata.sql
@@ -1,86 +1,85 @@
-FID,municipality,url,court_url,address,city,state,zip,phone_num,online_payment,dress_code,fine_schedule
+FID,municipality,url,court_url,address,city,state,zip,phone_num
 1,Ballwin,http://www.ballwin.mo.us,"http://www.ballwin.mo.us/City-Government/Departments/Municipal-Court
-","300 Park Drive","St. Louis",MO,63011,NULL,NULL,NULL,NULL
-2,"Bella Villa",http://www.cityofbellavilla-mo.org,NULL,"751 Avenue H","St. Louis",MO,63125,NULL,NULL,NULL,NULL
-3,"Bellefontaine Neighbors",NULL,NULL,"9641 Bellefontaine Road","St. Louis",MO,63137,NULL,NULL,NULL,NULL
-4,Bel-Nor,NULL,NULL,"8416 Natural Bridge Road","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-5,Bel-Ridge,NULL,NULL,"8920 Natural Bridge Road","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-6,"Berkeley 1",NULL,NULL,"6120 Madison","St. Louis",MO,63134,NULL,NULL,NULL,NULL
-7,"Berkeley 2",NULL,NULL,"6140 N Hanley Road","St. Louis",MO,63134,NULL,NULL,NULL,NULL
-8,"Beverly Hills",NULL,NULL,"7150 Natural Bridge Road","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-9,"Black Jack",NULL,NULL,"12500 Old Jamestown Road","St. Louis",MO,63033,NULL,NULL,NULL,NULL
-10,"Breckenridge Hills",NULL,NULL,"9623 St. Charles Rock Road","St. Louis",MO,63114,NULL,NULL,NULL,NULL
-11,Brentwood,NULL,NULL,"2348 South Brentwood Boulevard","St. Louis",MO,63144,NULL,NULL,NULL,NULL
-12,Bridgeton,NULL,NULL,"12355 Natural Bridge Road","St. Louis",MO,63044,NULL,NULL,NULL,NULL
-13,"Calverton Park",NULL,NULL,"52 Young Drive","St. Louis",MO,63135,NULL,NULL,NULL,NULL
-14,Charlack,NULL,NULL,"8401 Midland Boulevard","St. Louis",MO,63114,NULL,NULL,NULL,NULL
-15,Chesterfield,NULL,NULL,"690 Chesterfield Parkway West","St. Louis",MO,63017,NULL,NULL,NULL,NULL
-16,"Clarkson Valley",NULL,NULL,"15933 Clayton Road","St. Louis",MO,63011,NULL,NULL,NULL,NULL
-17,Clayton,NULL,NULL,"10 South Brentwood Boulevard","St. Louis",MO,63105,NULL,NULL,NULL,NULL
-18,"Cool Valley",NULL,NULL,"100 Signal Drive","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-19,"Country Club Hills",NULL,NULL,"7422 Eunice Avenue","St. Louis",MO,63136,NULL,NULL,NULL,NULL
-20,Crestwood,NULL,NULL,"1 Detjen Drive","St. Louis",MO,63126,NULL,NULL,NULL,NULL
-21,"Creve Coeur",NULL,NULL,"300 North New Ballas Road","St. Louis",MO,63141,NULL,NULL,NULL,NULL
-22,Dellwood,NULL,NULL,"1415 Chambers Road","St. Louis",MO,63135,NULL,NULL,NULL,NULL
-23,"Des Peres",NULL,NULL,"12325 Manchester Road","St. Louis",MO,63131,NULL,NULL,NULL,NULL
-24,Edmundson,NULL,NULL,"4440 Holman Lane","St. Louis",MO,63134,NULL,NULL,NULL,NULL
-25,Ellisville,NULL,NULL,"37 Weis Avenue","St. Louis",MO,63011,NULL,NULL,NULL,NULL
-26,Eureka,NULL,NULL,"120 City Hall Drive","St. Louis",MO,63025,NULL,NULL,NULL,NULL
-27,Fenton,NULL,NULL,"625 New Smizer Mill Road","St. Louis",MO,63026,NULL,NULL,NULL,NULL
-28,Ferguson,NULL,NULL,"110 Church Street","St. Louis",MO,63135,NULL,NULL,NULL,NULL
-29,"Flordell Hills",NULL,NULL,"7001 Brandon Avenue","St. Louis",MO,63136,NULL,NULL,NULL,NULL
-30,Florissant,NULL,NULL,"315 Howdershell Road","St. Louis",MO,63031,NULL,NULL,NULL,NULL
-31,Frontenac,NULL,NULL,"10555 Clayton Road","St. Louis",MO,63131,NULL,NULL,NULL,NULL
-32,Glendale,NULL,NULL,"424 North Sappington Road","St. Louis",MO,63122,NULL,NULL,NULL,NULL
-33,"Grantwood Village",NULL,NULL,"One Missionary Way","St. Louis",MO,63123,NULL,NULL,NULL,NULL
-34,Greendale,NULL,NULL,"7700 Natural Bridge Road","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-35,"Hanley Hills",NULL,NULL,"7713 Utica Drive","St. Louis",MO,63133,NULL,NULL,NULL,NULL
-36,Hazelwood,NULL,NULL,"415 Elm Grove Lane","St. Louis",MO,63042,NULL,NULL,NULL,NULL
-37,Hillsdale,NULL,NULL,"6428 Jesse Jackson Avenue","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-38,Jennings,NULL,NULL,"7005 Florence Avenue","St. Louis",MO,63136,NULL,NULL,NULL,NULL
-39,Kinloch,NULL,NULL,"8140 Scott","St. Louis",MO,63140,NULL,NULL,NULL,NULL
-40,Kirkwood,NULL,NULL,"139 S. Kirkwood Road","St. Louis",MO,63122,NULL,NULL,NULL,NULL
-41,Ladue,NULL,NULL,"9435 Clayton Road","St. Louis",MO,63124,NULL,NULL,NULL,NULL
-42,Lakeshire,NULL,NULL,"10000 Puttington Drive","St. Louis",MO,63123,NULL,NULL,NULL,NULL
-43,Mackenzie,NULL,NULL,"4400 Shrewsbury Avenue","St. Louis",MO,63119,NULL,NULL,NULL,NULL
-44,Manchester,NULL,NULL,"200 Highlands Blvd. Dr.","St. Louis",MO,63011,NULL,NULL,NULL,NULL
-45,Maplewood,NULL,NULL,"7601 Manchester Road","St. Louis",MO,63143,NULL,NULL,NULL,NULL
-46,Marlborough,NULL,NULL,"7826 Wimbledon Drive","St. Louis",MO,63119,NULL,NULL,NULL,NULL
-47,"Maryland Heights",NULL,NULL,"11911 Dorsett Road","St. Louis",MO,63043,NULL,NULL,NULL,NULL
-48,"Moline Acres",NULL,NULL,"2449 Chambers Road","St. Louis",MO,63136,NULL,NULL,NULL,NULL
-49,Normandy,NULL,NULL,"7700 Natural Bridge Road","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-50,Northwoods,NULL,NULL,"4600 Oakridge Boulevard","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-51,Oakland,NULL,NULL,"1320 W. Lockwood Avenue","St. Louis",MO,63122,NULL,NULL,NULL,NULL
-52,Olivette,NULL,NULL,"9473 Olive Boulevard","St. Louis",MO,63132,NULL,NULL,NULL,NULL
-53,Overland,NULL,NULL,"2410 Goodale Avenue","St. Louis",MO,63114,NULL,NULL,NULL,NULL
-54,Pacific,NULL,NULL,"300 Hoven Drive","St. Louis",MO,63079,NULL,NULL,NULL,NULL
-55,Pagedale,NULL,NULL,"1420 Ferguson Avenue","St. Louis",MO,63133,NULL,NULL,NULL,NULL
-56,"Pasadena Hills",NULL,NULL,"3915 Roland Boulevard","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-57,"Pasadena Park",NULL,NULL,"7615 South Sunset","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-58,"Pine Lawn",NULL,NULL,"6250 Steve Marve Avenue","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-59,"Richmond Heights",NULL,NULL,"7447 Dale Avenue","St. Louis",MO,63117,NULL,NULL,NULL,NULL
-60,Riverview,NULL,NULL,"9699 Lilac Drive","St. Louis",MO,63137,NULL,NULL,NULL,NULL
-61,"Rock Hill",NULL,NULL,"320 West Thornton Avenue","St. Louis",MO,63119,NULL,NULL,NULL,NULL
-62,Shrewsbury,NULL,NULL,"4400 Shrewsbury Avenue","St. Louis",MO,63119,NULL,NULL,NULL,NULL
-63,"St. Ann",NULL,NULL,"10405 St. Charles Rock Road","St. Louis",MO,63074,NULL,NULL,NULL,NULL
-64,"St. John",NULL,NULL,"8944 St. Charles Rock Road","St. Louis",MO,63114,NULL,NULL,NULL,NULL
-65,"Sunset Hills",NULL,NULL,"3939 South Lindbergh","St. Louis",MO,63127,NULL,NULL,NULL,NULL
-66,"Sycamore Hills",NULL,NULL,"8944 St. Charles Rock Road","St. Louis",MO,63114,NULL,NULL,NULL,NULL
-67,"Town and Country",NULL,NULL,"1011 Municipal Center Drive","St. Louis",MO,63131,NULL,NULL,NULL,NULL
-68,"University City",NULL,NULL,"975 Pennsylvania Avenue","St. Louis",MO,63130,NULL,NULL,NULL,NULL
-69,"Uplands Park",NULL,NULL,"6390 Natural Bridge Road","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-70,"Valley Park",NULL,NULL,"320 Benton Street","St. Louis",MO,63088,NULL,NULL,NULL,NULL
-71,"Velda City",NULL,NULL,"2560 Lucas & Hunt Road","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-72,"Velda Village Hills",NULL,NULL,"3501 Avondale Avenue","St. Louis",MO,63121,NULL,NULL,NULL,NULL
-73,"Vinita Park",NULL,NULL,"8374 Midland Boulevard","St. Louis",MO,63114,NULL,NULL,NULL,NULL
-74,"Vinita Terrace",NULL,NULL,"8027 Page Boulevard","St. Louis",MO,63130,NULL,NULL,NULL,NULL
-75,"Warson Woods",NULL,NULL,"10015 Manchester Road","St. Louis",MO,63122,NULL,NULL,NULL,NULL
-76,"Webster Groves",NULL,NULL,"4 Lockwood Aveue","St. Louis",MO,63119,NULL,NULL,NULL,NULL
-77,Wellston,NULL,NULL,"1414 Evergreen","St. Louis",MO,63133,NULL,NULL,NULL,NULL
-78,Wildwood,NULL,NULL,"16860 Main Street","St. Louis",MO,63040,NULL,NULL,NULL,NULL
-79,Winchester,NULL,NULL,"109 Lindy Boulevard","St. Louis",MO,63021,NULL,NULL,NULL,NULL
-80,"Woodson Terrace",NULL,NULL,"4323 Woodson Road","St. Louis",MO,63134,NULL,NULL,NULL,NULL
-81,"Unincorporated Central St. Louis County",NULL,NULL,"7900 Carondelet Avenue","St. Louis",MO,63105,NULL,NULL,NULL,NULL
-82,"Unincorporated West St. Louis County",NULL,NULL,"82 Clarkson Wilson Centre","St. Louis",MO,63017,NULL,NULL,NULL,NULL
-83,"Unincorporated North St. Louis County",NULL,NULL,"21 Village Square","St. Louis",MO,63042,NULL,NULL,NULL,NULL
-84,"Unincorporated South St. Louis County",NULL,NULL,"4544 Lemay Ferry Road","St. Louis",MO,63129,NULL,NULL,NULL,NULL
+","300 Park Drive","St. Louis",MO,63011,NULL
+2,"Bella Villa",http://www.cityofbellavilla-mo.org,NULL,"751 Avenue H","St. Louis",MO,63125,NULL
+3,"Bellefontaine Neighbors",http://www.cityofbn.com,http://www.cityofbn.com/departments/court/,"9641 Bellefontaine Road","St. Louis",MO,63137,NULL
+4,Bel-Nor,NULL,NULL,"8416 Natural Bridge Road","St. Louis",MO,63121,NULL
+5,Bel-Ridge,http://bel-ridge.us,http://bel-ridge.us/?page_id=90,"8920 Natural Bridge Road","St. Louis",MO,63121,NULL
+6,"Berkeley 1","http://www.cityofberkeley.us ",http://www.cityofberkeley.us/index.aspx?nid=124,"6120 Madison","St. Louis",MO,63134,NULL
+8,"Beverly Hills",,,"7150 Natural Bridge Road","St. Louis",MO,63121,NULL
+9,"Black Jack",http://www.cityofblackjack.com,http://www.cityofblackjack.com/241/Municipal-Court,"12500 Old Jamestown Road","St. Louis",MO,63033,NULL
+10,"Breckenridge Hills",http://www.breckenridge-hills.mo.us,NULL,"9623 St. Charles Rock Road","St. Louis",MO,63114,NULL
+11,Brentwood,http://www.brentwoodmo.org,http://www.brentwoodmo.org/index.aspx?nid=477,"2348 South Brentwood Boulevard","St. Louis",MO,63144,NULL
+12,Bridgeton,http://www.bridgetonmo.com,http://www.bridgetonmo.com/departments/municipal-court,"12355 Natural Bridge Road","St. Louis",MO,63044,NULL
+13,"Calverton Park",http://www.calvertonparkmo.com,http://www.calvertonparkmo.com/village-court/,"52 Young Drive","St. Louis",MO,63135,NULL
+14,Charlack,http://www.cityofcharlack.com,http://www.cityofcharlack.com/#!municipal-court/c1g4i,"8401 Midland Boulevard","St. Louis",MO,63114,NULL
+15,Chesterfield,http://www.chesterfield-mo.us,http://www.chesterfield.mo.us/municipal-court.html,"690 Chesterfield Parkway West","St. Louis",MO,63017,NULL
+16,"Clarkson Valley",http://www.clarksonvalley.org/,http://www.clarksonvalley.org/traffic.html,"15933 Clayton Road","St. Louis",MO,63011,NULL
+17,Clayton,http://www.claytonmo.gov/,http://www.claytonmo.gov/Government/Municipal_Court.htm?,"10 South Brentwood Boulevard","St. Louis",MO,63105,NULL
+18,"Cool Valley",http://www.cityofcoolvalley.com/,http://www.cityofcoolvalley.com/index.php/departments/municipal-court,"100 Signal Drive","St. Louis",MO,63121,NULL
+19,"Country Club Hills",NULL,NULL,"7422 Eunice Avenue","St. Louis",MO,63136,NULL
+20,Crestwood,http://www.cityofcrestwood.org/,http://www.cityofcrestwood.org/department/index.php?structureid=19,"1 Detjen Drive","St. Louis",MO,63126,NULL
+21,"Creve Coeur",http://www.creve-coeur.org/,http://www.creve-coeur.org/index.aspx?nid=101,"300 North New Ballas Road","St. Louis",MO,63141,NULL
+22,Dellwood,http://www.cityofdellwoodmo.com/,http://www.cityofdellwoodmo.com/563/Municipal-Court,"1415 Chambers Road","St. Louis",MO,63135,NULL
+23,"Des Peres",http://www.desperesmo.org/,http://www.desperesmo.org/Index.aspx?NID=96,"12325 Manchester Road","St. Louis",MO,63131,NULL
+24,Edmundson,http://www.cityofedmundson.com/,http://www.cityofedmundson.com/index.php?option=com_content&view=article&id=7&Itemid=115,"4440 Holman Lane","St. Louis",MO,63134,NULL
+25,Ellisville,http://www.ellisville.mo.us/,http://www.ellisville.mo.us/165/Municipal-Court,"37 Weis Avenue","St. Louis",MO,63011,NULL
+26,Eureka,http://www.eureka.mo.us/,http://www.eureka.mo.us/departments/municipal-court/,"120 City Hall Drive","St. Louis",MO,63025,NULL
+27,Fenton,http://fentonmo.org/,http://fentonmo.org/index.aspx?nid=563,"625 New Smizer Mill Road","St. Louis",MO,63026,NULL
+28,Ferguson,http://www.fergusoncity.com/,http://www.fergusoncity.com/60/The-City-Of-Ferguson-Municipal-Courts,"110 Church Street","St. Louis",MO,63135,NULL
+29,"Flordell Hills",http://flordell.com/,NULL,"7001 Brandon Avenue","St. Louis",MO,63136,NULL
+30,Florissant,http://www.florissantmo.com/,http://www.florissantmo.com/department/index.php?structureid=19,"315 Howdershell Road","St. Louis",MO,63031,NULL
+31,Frontenac,http://www.cityoffrontenac.org/,http://www.cityoffrontenac.org/index.php?id=56,"10555 Clayton Road","St. Louis",MO,63131,NULL
+32,Glendale,http://www.glendalemo.org/,http://www.glendalemo.org/index.php?option=com_content&view=article&id=11&Itemid=18,"424 North Sappington Road","St. Louis",MO,63122,NULL
+33,"Grantwood Village",http://www.grantwoodvillage.org/,NULL,"One Missionary Way","St. Louis",MO,63123,NULL
+34,Greendale,http://www.greendale-mo.us/,NULL,"7700 Natural Bridge Road","St. Louis",MO,63121,NULL
+35,"Hanley Hills",NULL,NULL,"7713 Utica Drive","St. Louis",MO,63133,NULL
+36,Hazelwood,http://www.hazelwoodmo.org/,http://www.hazelwoodmo.org/city-government/departments/courts,"415 Elm Grove Lane","St. Louis",MO,63042,NULL
+37,Hillsdale,NULL,NULL,"6428 Jesse Jackson Avenue","St. Louis",MO,63121,NULL
+38,Jennings,http://www.cityofjennings.org/,http://www.cityofjennings.org/court.html,"7005 Florence Avenue","St. Louis",MO,63136,NULL
+39,Kinloch,NULL,NULL,"8140 Scott","St. Louis",MO,63140,NULL
+40,Kirkwood,http://www.kirkwoodmo.org/,http://www.kirkwoodmo.org/content/City-Departments/2040/municipal-court.aspx,"139 S. Kirkwood Road","St. Louis",MO,63122,NULL
+41,Ladue,http://www.cityofladue-mo.gov/,http://www.cityofladue-mo.gov/departments/municipal-court-196,"9435 Clayton Road","St. Louis",MO,63124,NULL
+42,Lakeshire,http://www.lakeshiremo.com/,http://www.lakeshiremo.com/court-procedures/,"10000 Puttington Drive","St. Louis",MO,63123,NULL
+43,Mackenzie,NULL,NULL,"4400 Shrewsbury Avenue","St. Louis",MO,63119,NULL
+44,Manchester,http://www.manchestermo.gov/,http://www.manchestermo.gov/index.asp?SEC=0BE94A96-7F3E-4567-A282-BB5616082CA4&Type=B_BASIC,"200 Highlands Blvd. Dr.","St. Louis",MO,63011,NULL
+45,Maplewood,http://www.cityofmaplewood.com/,http://www.cityofmaplewood.com/index.aspx?nid=66,"7601 Manchester Road","St. Louis",MO,63143,NULL
+46,Marlborough,http://www.villageofmarlborough.com/Pages/default.aspx,http://www.villageofmarlborough.com/Pages/MunicipalCourt.aspx,"7826 Wimbledon Drive","St. Louis",MO,63119,NULL
+47,"Maryland Heights",http://www.marylandheights.com/,http://www.marylandheights.com/index.aspx?page=78,"11911 Dorsett Road","St. Louis",MO,63043,NULL
+48,"Moline Acres",http://www.molineacres.org/,http://www.molineacres.org/Courts.aspx,"2449 Chambers Road","St. Louis",MO,63136,NULL
+49,Normandy,http://www.cityofnormandy.gov/,http://www.cityofnormandy.gov/index.aspx?nid=563,"7700 Natural Bridge Road","St. Louis",MO,63121,NULL
+50,Northwoods,http://www.cityofnorthwoods.com/,NULL,"4600 Oakridge Boulevard","St. Louis",MO,63121,NULL
+51,Oakland,http://www.oaklandmo.org/,http://www.oaklandmo.org/municipal-court.aspx,"1320 W. Lockwood Avenue","St. Louis",MO,63122,NULL
+52,Olivette,http://www.olivettemo.com/,http://www.olivettemo.com/pView.aspx?id=2037&catid=29,"9473 Olive Boulevard","St. Louis",MO,63132,NULL
+53,Overland,http://www.overlandmo.org/,http://www.overlandmo.org/58/Overland-Municipal-Court,"2410 Goodale Avenue","St. Louis",MO,63114,NULL
+54,Pacific,http://www.pacificmissouri.com/,,"300 Hoven Drive","St. Louis",MO,63079,NULL
+55,Pagedale,http://www.cityofpagedale.com/,http://www.cityofpagedale.com/#!municipal-court/cme6,"1420 Ferguson Avenue","St. Louis",MO,63133,NULL
+56,"Pasadena Hills",http://www.pasadenahills.com/,NULL,"3915 Roland Boulevard","St. Louis",MO,63121,NULL
+57,"Pasadena Park",NULL,NULL,"7615 South Sunset","St. Louis",MO,63121,NULL
+58,"Pine Lawn",http://www.pinelawn.org/,http://www.pinelawn.org/#!court/c78g,"6250 Steve Marve Avenue","St. Louis",MO,63121,NULL
+59,"Richmond Heights",http://www.richmondheights.org/,http://www.richmondheights.org/departments/court/index.php,"7447 Dale Avenue","St. Louis",MO,63117,NULL
+60,Riverview,http://www.riverviewmo.org/,http://www.riverviewmo.org/Courts.aspx,"9699 Lilac Drive","St. Louis",MO,63137,NULL
+61,"Rock Hill",http://www.rockhillmo.net/,http://www.rockhillmo.net/Courts.aspx,"320 West Thornton Avenue","St. Louis",MO,63119,NULL
+62,Shrewsbury,http://www.cityofshrewsbury.com/,http://www.cityofshrewsbury.com/municipal-courts.htm,"4400 Shrewsbury Avenue","St. Louis",MO,63119,NULL
+63,"St. Ann",http://www.stannmo.org/,http://www.stannmo.org/369/Municipal-Court,"10405 St. Charles Rock Road","St. Louis",MO,63074,NULL
+64,"St. John",http://www.cityofstjohn.org/,http://www.cityofstjohn.org/index.php/municipal-court.html,"8944 St. Charles Rock Road","St. Louis",MO,63114,NULL
+65,"Sunset Hills",http://www.sunset-hills.com/,http://www.sunset-hills.com/148/Municipal-Court,"3939 South Lindbergh","St. Louis",MO,63127,NULL
+66,"Sycamore Hills",http://www.villageofsycamorehills.com/,NULL,"8944 St. Charles Rock Road","St. Louis",MO,63114,NULL
+67,"Town and Country",http://www.town-and-country.org/,http://www.town-and-country.org/150/Municipal-Court,"1011 Municipal Center Drive","St. Louis",MO,63131,NULL
+68,"University City",http://www.ucitymo.org/,http://www.ucitymo.org/index.aspx?nid=82,"975 Pennsylvania Avenue","St. Louis",MO,63130,NULL
+69,"Uplands Park",,NULL,"6390 Natural Bridge Road","St. Louis",MO,63121,NULL
+70,"Valley Park",http://www.valleyparkmo.org/,NULL,"320 Benton Street","St. Louis",MO,63088,NULL
+71,"Velda City",http://www.veldacity.org/,http://www.veldacity.org/departments/municipal-court,"2560 Lucas & Hunt Road","St. Louis",MO,63121,NULL
+72,"Velda Village Hills",NULL,NULL,"3501 Avondale Avenue","St. Louis",MO,63121,NULL
+73,"Vinita Park",http://www.vinitapark.org/,http://www.vinitapark.org/index.php/city-departments/court-information,"8374 Midland Boulevard","St. Louis",MO,63114,NULL
+74,"Vinita Terrace",,,"8027 Page Boulevard","St. Louis",MO,63130,NULL
+75,"Warson Woods",http://www.warsonwoods.com/,http://www.warsonwoods.com/Templates/Warson%20Woods%20City%20Hall%203.5%20(Court%20Page).html,"10015 Manchester Road","St. Louis",MO,63122,NULL
+76,"Webster Groves",http://www.webstergroves.org/,http://www.webstergroves.org/index.aspx?nid=239,"4 Lockwood Aveue","St. Louis",MO,63119,NULL
+77,Wellston,NULL,NULL,"1414 Evergreen","St. Louis",MO,63133,NULL
+78,Wildwood,http://www.cityofwildwood.com/,http://www.cityofwildwood.com/135/Municipal-Court,"16860 Main Street","St. Louis",MO,63040,NULL
+79,Winchester,http://www.city.winchester.mo.us/,http://www.city.winchester.mo.us/Municipal-Court/,"109 Lindy Boulevard","St. Louis",MO,63021,NULL
+80,"Woodson Terrace",http://woodsonterrace.net/,http://woodsonterrace.net/City-Clerk/Municipal-Court-Information-Online-Payment-Link/,"4323 Woodson Road","St. Louis",MO,63134,NULL
+81,"Unincorporated Central St. Louis County",http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts,http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts/LocationsandHours/CentralDivision,"7900 Carondelet Avenue","St. Louis",MO,63105,NULL
+82,"Unincorporated West St. Louis County",http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts,http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts/LocationsandHours/WestDivision,"82 Clarkson Wilson Centre","St. Louis",MO,63017,NULL
+83,"Unincorporated North St. Louis County",http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts,http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts/LocationsandHours/NorthDivision,"21 Village Square","St. Louis",MO,63042,NULL
+84,"Unincorporated South St. Louis County",http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts,http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts/LocationsandHours/SouthDivision,"4544 Lemay Ferry Road","St. Louis",MO,63129,NULL
